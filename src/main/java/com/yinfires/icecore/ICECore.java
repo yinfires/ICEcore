88b39dbdb397d18d07f23392a26b4af6e64fa32c
yinfires <2199153624@qq.com>
2026-08-18 15:43:43 +0800

    初立项

---
+++ src/main/java/com/yinfires/icecore/ICECore.java
@@ -0,0 +1,11 @@
+package com.yinfires.icecore;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(ICECore.MOD_ID)
+public final class ICECore {
+    public static final String MOD_ID = "icecore";
+
+    public ICECore() {
+    }
+}
